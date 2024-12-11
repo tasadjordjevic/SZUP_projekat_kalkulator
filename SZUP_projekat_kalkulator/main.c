03bca7a689b4e91c791d6b623d028f234e2cee02
anastasiadj <anastasiadj19@gmail.com>
2024-12-11 12:21:48 +0100

    Poboljsanje postojeceg koda i dodavanje opcija za mnozenje i deljenje

--- SZUP_projekat_kalkulator/main.c
+++ SZUP_projekat_kalkulator/main.c
@@ -1,30 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int sabiranje (int x, int y) {
+float sabiranje (float x, float y) {
     return x+y;
 }
 
-int oduzimanje (int x, int y) {
+float oduzimanje (float x, float y) {
     return x-y;
 }
 
+float mnozenje (float x, float y) {
+    return x*y;
+}
+
+float deljenje (float x, float y) {
+    return x/y;
+}
 int main()
 {
-    int a, b, izbor, rezultat;
+    int izbor;
+    float a, b, rezultat;
     printf("Unesite prvi broj\n");
-    scanf("%d", &a);
+    scanf("%f", &a);
     printf("Unesite drugi broj\n");
-    scanf("%d", &b);
-    printf("Odaberite operaciju\n1. Sabiranje\n2. Oduzimanje\n");
+    scanf("%f", &b);
+    printf("Odaberite operaciju\n1. Sabiranje\n2. Oduzimanje\n3. Mnozenje\n4. Deljenje\n");
     scanf("%d", &izbor);
     if(izbor==1) {
         rezultat=sabiranje(a,b);
-        printf("Rezultat sabiranja: %d\n", rezultat);
+        printf("Rezultat sabiranja: %f\n", rezultat);
     }
     else if(izbor==2) {
         rezultat=oduzimanje(a,b);
-        printf("Rezultat oduzimanja: %d\n", rezultat);
+        printf("Rezultat oduzimanja: %f\n", rezultat);
+    }
+    else if(izbor==3) {
+        rezultat=mnozenje(a,b);
+        printf("Rezultat mnozenja: %f\n", rezultat);
+    }
+    else if(izbor==4) {
+        rezultat=deljenje(a,b);
+        printf("Rezultat deljenja: %f\n", rezultat);
     }
     else {
         printf("Neispravan izbor\n");
