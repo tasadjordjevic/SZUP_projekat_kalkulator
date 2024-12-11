6738bb1dccdf3fa70fea223c3d4ccd076227677b
anastasiadj <anastasiadj19@gmail.com>
2024-12-11 12:11:58 +0100

    Dodavanje funkcija za sabiranje i oduzimanje

--- SZUP_projekat_kalkulator/main.c
+++ SZUP_projekat_kalkulator/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int sabiranje (int x, int y) {
+    return x+y;
+}
+
+int oduzimanje (int x, int y) {
+    return x-y;
+}
+
 int main()
 {
     int a, b, izbor, rezultat;
@@ -11,11 +19,11 @@ int main()
     printf("Odaberite operaciju\n1. Sabiranje\n2. Oduzimanje\n");
     scanf("%d", &izbor);
     if(izbor==1) {
-        rezultat=a+b;
+        rezultat=sabiranje(a,b);
         printf("Rezultat sabiranja: %d\n", rezultat);
     }
     else if(izbor==2) {
-        rezultat=a-b;
+        rezultat=oduzimanje(a,b);
         printf("Rezultat oduzimanja: %d\n", rezultat);
     }
     else {
