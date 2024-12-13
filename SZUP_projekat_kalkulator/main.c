fe5acec7ca56673e8616e25ea9d2672476102def
anastasiadj <anastasiadj19@gmail.com>
2024-12-13 13:38:31 +0100

    Poboljsanje koda

--- SZUP_projekat_kalkulator/main.c
+++ SZUP_projekat_kalkulator/main.c
@@ -1,41 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include "kalkulator.h"
 
-
-float sabiranje (float x, float y) {
-    return x+y;
-}
-
-float oduzimanje (float x, float y) {
-    return x-y;
-}
-
-float mnozenje (float x, float y) {
-    return x*y;
-}
-
-float deljenje (float x, float y) {
-    return x/y;
-}
-
-float ostatak (int x, int y) {
-    return x%y;
-}
-float anab (float x, float y) {
-    return pow(x,y);
-}
-
-float bnaa (float x, float y) {
-    return pow(y,x);
-}
-
-float koren (float x) {
-    return sqrt(x);
-}
-
-int main()
-{
+int main() {
     int izbor;
     float a, b, rezultat;
     printf("Unesite prvi broj\n");
@@ -44,44 +10,20 @@ int main()
     scanf("%f", &b);
     printf("Odaberite operaciju\n1. Sabiranje\n2. Oduzimanje\n3. Mnozenje\n4. Deljenje\n5. Ostatak pri deljenju\n6. a^b\n7. b^a\n8. Koren iz a\n9. Koren iz b\n");
     scanf("%d", &izbor);
-    if(izbor==1) {
-        rezultat=sabiranje(a,b);
-        printf("Rezultat sabiranja: %f\n", rezultat);
-    }
-    else if(izbor==2) {
-        rezultat=oduzimanje(a,b);
-        printf("Rezultat oduzimanja: %f\n", rezultat);
-    }
-    else if(izbor==3) {
-        rezultat=mnozenje(a,b);
-        printf("Rezultat mnozenja: %f\n", rezultat);
-    }
-    else if(izbor==4) {
-        rezultat=deljenje(a,b);
-        printf("Rezultat deljenja: %f\n", rezultat);
-    }
-    else if(izbor==5) {
-        rezultat=ostatak(a,b);
-        printf("Ostatak pri deljenju: %d\n", (int)rezultat);
-    }
-    else if(izbor==6) {
-        rezultat=anab(a,b);
-        printf("a^b: %f\n", rezultat);
-    }
-    else if(izbor==7) {
-        rezultat=bnaa(a,b);
-        printf("b^a: %f\n", rezultat);
-    }
-    else if(izbor==8) {
-        rezultat=koren(a);
-        printf("Koren broja a: %f\n", rezultat);
-    }
-    else if(izbor==9) {
-        rezultat=koren(b);
-        printf("Koren broja b: %f\n", rezultat);
-    }
-    else {
-        printf("Neispravan izbor\n");
+    switch (izbor) {
+        case 1: rezultat = sabiranje(a, b); printf("Rezultat sabiranja: %.2f\n", rezultat); break;
+        case 2: rezultat = oduzimanje(a, b); printf("Rezultat oduzimanja: %.2f\n", rezultat); break;
+        case 3: rezultat = mnozenje(a, b); printf("Rezultat mnozenja: %.2f\n", rezultat); break;
+        case 4:
+            if (b != 0) { rezultat = deljenje(a, b); printf("Rezultat deljenja: %.2f\n", rezultat); }
+            else { printf("Greska: Deljenje nulom nije dozvoljeno.\n"); }
+            break;
+        case 5: rezultat = ostatak((int)a, (int)b); printf("Ostatak pri deljenju: %d\n", (int)rezultat); break;
+        case 6: rezultat = anab(a, b); printf("a^b: %.2f\n", rezultat); break;
+        case 7: rezultat = bnaa(a, b); printf("b^a: %.2f\n", rezultat); break;
+        case 8: rezultat = koren(a); printf("Koren broja a: %.2f\n", rezultat); break;
+        case 9: rezultat = koren(b); printf("Koren broja b: %.2f\n", rezultat); break;
+        default: printf("Neispravan izbor\n"); break;
     }
     return 0;
 }
